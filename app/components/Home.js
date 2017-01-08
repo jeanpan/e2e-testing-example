d896a746795bd0c23f9861c637e4797c9d5ab952
jeanpan <beats.1213@gmail.com>
2017-01-08 12:27:38 +0800

    add .slugignore

--- app/components/Home.js
+++ app/components/Home.js
@@ -15,20 +15,6 @@ class Home extends React.Component {
               mollis euismod. Donec sed odio dui.</p>
             <a href="#" role="button">View details</a>
           </div>
-          <div className="col-sm">
-            <h3>Heading</h3>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-              mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-              mollis euismod. Donec sed odio dui.</p>
-            <a href="#" role="button">View details</a>
-          </div>
-          <div className="col-sm">
-            <h3>Heading</h3>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-              mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-              mollis euismod. Donec sed odio dui.</p>
-            <a href="#" role="button">View details</a>
-          </div>
         </div>
       </div>
     );
